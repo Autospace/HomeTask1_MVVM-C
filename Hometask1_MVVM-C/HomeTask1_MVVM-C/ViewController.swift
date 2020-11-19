b6744a73316bde1572a705cc8962ac9231f17bdd
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-11-20 00:15:37 +0300

    Initial new project

---
+++ Hometask1_MVVM-C/HomeTask1_MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask1_MVVM-C
+//
+//  Created by Alex Mostovnikov on 20/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
